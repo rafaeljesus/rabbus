23229093cfb9ed48b170d76fd8e4b8d1726502ba
Rafael Jesus <rafaelljesus86@gmail.com>
2017-06-11 20:55:53 +0200

    Improve docs

--- circuitbreaker/circuitbreaker.go
+++ circuitbreaker/circuitbreaker.go
@@ -9,6 +9,7 @@ import (
 
 type function func() error
 
+// Breaker exposes a interface with the circuit breaker public methods.
 type Breaker interface {
 	Call(fn function, timeout time.Duration) error
 }
@@ -19,12 +20,16 @@ type breaker struct {
 	sleep    time.Duration
 }
 
+// NewThresholdBreaker creates a circuit breaker.
 func NewThresholdBreaker(threshold int64, attempts int, sleep time.Duration) Breaker {
 	s := time.Second * time.Duration(sleep)
 	cb := circuit.NewThresholdBreaker(threshold)
 	return &breaker{cb, attempts, s}
 }
 
+// Call wraps the function protected by the circuit breaker with a retry mechanism.
+// A failure is recorded whenever the function returns an error. If the called function takes longer
+// than timeout to run, a failure will be recorded.
 func (b *breaker) Call(fn function, timeout time.Duration) error {
 	return retry.Do(func() error {
 		return b.provider.Call(func() error {
